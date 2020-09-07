e6f4d3aca87413847d873e9f6e8c44ebf934aa87
buendia <897355578@qq.com>
2020-09-07 20:58:04 +0800

    init

---
+++ src/main/java/cn/buendia/blackbooks/BlackbooksApplication.java
@@ -0,0 +1,13 @@
+package cn.buendia.blackbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/buendia/blackbooks/BlackbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.buendia.blackbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
